5319ab0c5a4b81369a8343c083c10d5e34633d30
seon <seonu0823@gmail.com>
2019-07-30 21:38:09 +0900

    #2-1 2D Context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.strokeStyle = '#2C2C2C';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,9 +10,21 @@ function stopPainting() {
     painting = false;
 };
 
+function startPainting() {
+    painting = true;
+}
+
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 };
 
 function onMouseDown(event) {
@@ -21,7 +37,7 @@ function onMouseUp(event) {
 
 if(canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
 }
